bd5d93f2b0203105299d6106ae50d1ec26cc11f5
Lajili Dhia <dhiaelajili4@gmail.com>
2022-04-16 01:19:26 +0100

    adding encore and yarn

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
