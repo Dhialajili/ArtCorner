33ea18bcc1b1b446a52da4b393c72c5429cfad1e
Lajili Dhia <dhiaelajili4@gmail.com>
2022-05-26 13:30:44 +0100

    nothing has changed

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArtWorkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(ArtWorkRepository $artWorkRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'art_works' => $artWorkRepository->findAll(),
         ]);
     }
 }
